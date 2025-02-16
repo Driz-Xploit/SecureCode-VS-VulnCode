25c36182a5a372918f28dc212c524e6c57cbc816
Driz <davidkzn8@gmail.com>
2025-02-16 03:07:10 +0000

    php

--- PHP/Sanitazing_Input1/secure.php
+++ PHP/Sanitazing_Input1/secure.php
@@ -1,7 +1,7 @@
 <html>
     <head>
         <title>
-            Vuln CODE
+            Secure Code
         </title>
         <style>
             .content {
@@ -12,13 +12,13 @@
     </head>
     <body style="background-color: black;">
         <div class="content">
-            <h1>Vuln CODE</h1><hr>
+            <h1>Secure CODE</h1><hr>
             <p>HI It's From PHP, How are you?</p>
             <form action="vuln.php" method="get">
                 <input type="text" name="text"><input type="submit">
             </form>
             <?php 
-            $TEXT = $_GET['text'];
+            $TEXT = htmlspecialchars($_GET['text'], ENT_QUOTES, 'UTF-8');
             if ($TEXT == '') {
                 echo "<hr><br>Your Input, Please<br><br><img src='https://i.pinimg.com/736x/1e/e5/b9/1ee5b974c1b0df8e046fb70b5deb7a96.jpg' width='200' height='200'";
             }
